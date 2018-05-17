69dc5f4d4c834a0f373c7368c9fa2c0b697eb450
Noman Rauf <nomi589@hotmail.com>
2018-05-17 19:21:25 +0300

    Successfully embed google maps through api call

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    this.setState({
+      map: this.initialMap()
+    });
+  }
+
+  initialMap() {
+    return new window.google.maps.Map(document.getElementById('map'), {
+      center: { lat: 31.588185, lng: 74.312794 },
+      zoom: 16
+    });
+  }
+
   render() {
-    return 'hello world';
+    return <div id="map" />;
   }
 }
 
