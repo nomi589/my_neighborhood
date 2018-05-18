103396ce6d791be59731cea42f21301b0fc17512
Noman Rauf <nomi589@hotmail.com>
2018-05-18 03:19:45 +0300

    Add initial markers on the map

--- src/App.js
+++ src/App.js
@@ -1,6 +1,45 @@
 import React from 'react';
 import Navbar from './Navbar';
 import List from './List';
+import Map from './Map';
+
+const placesOfInterest = [
+  {
+    name: 'Sheesh Mahal',
+    position: {
+      lat: 31.590029,
+      lng: 74.313102
+    }
+  },
+  {
+    name: 'Shrine of Baba Ghulam',
+    position: {
+      lat: 31.591354,
+      lng: 74.315859
+    }
+  },
+  {
+    name: 'Badshahi Mosque',
+    position: {
+      lat: 31.588334,
+      lng: 74.310753
+    }
+  },
+  {
+    name: 'Lahore Fort',
+    position: {
+      lat: 31.588151,
+      lng: 74.315109
+    }
+  },
+  {
+    name: 'Haveli Maharani Jindan',
+    position: {
+      lat: 31.589239,
+      lng: 74.313714
+    }
+  }
+];
 
 class App extends React.Component {
   state = {
@@ -8,32 +47,56 @@ class App extends React.Component {
   };
 
   componentDidMount() {
-    this.setState({
-      map: this.initialMap()
-    });
+    this.initializeMap();
   }
 
-  initialMap() {
-    return new window.google.maps.Map(document.getElementById('map'), {
+  initializeMap() {
+    let map = new window.google.maps.Map(document.getElementById('map'), {
       center: { lat: 31.588185, lng: 74.312794 },
       zoom: 16
     });
+
+    this.placeMarkers(placesOfInterest, map);
+    this.setState({ map });
+  }
+
+  clearCurrentMarkers() {
+    if (this.state.markers) {
+      this.state.markers.forEach(marker => {
+        marker.setMap(null);
+      });
+    }
+  }
+
+  placeMarkers(locations, map = this.state.map) {
+    let markers = [];
+
+    this.clearCurrentMarkers();
+
+    locations.forEach(location => {
+      markers.push(
+        new window.google.maps.Marker({
+          position: location.position,
+          map: map
+        })
+      );
+    });
+
+    this.setState({ markers });
   }
 
   toggleList(event) {
     this.state.viewList
       ? this.setState({ viewList: false })
       : this.setState({ viewList: true });
-
-    console.log(this.state.viewList);
   }
 
   render() {
     return (
       <div className="container">
         <Navbar toggleList={this.toggleList.bind(this)} />
-        {this.state.viewList && <List map={this.state.map} />}
-        <div id="map" />
+        {this.state.viewList && <List locations={placesOfInterest} />}
+        <Map />
       </div>
     );
   }
--- src/List.js
+++ src/List.js
@@ -1,38 +1,14 @@
 import React from 'react';
 
-const placesOfInterest = [
-  {
-    name: 'Sheesh Mahal',
-    lat: 31.590029,
-    lng: 74.313102
-  },
-  {
-    name: 'Shrine of Baba Ghulam',
-    lat: 31.591354,
-    lng: 74.315859
-  },
-  {
-    name: 'Badshahi Mosque',
-    lat: 31.588334,
-    lng: 74.310753
-  },
-  {
-    name: 'Lahore Fort',
-    lat: 31.588151,
-    lng: 74.315109
-  },
-  {
-    name: 'Haveli Maharani Jindan',
-    lat: 31.589239,
-    lng: 74.313714
-  }
-];
-
 class List extends React.Component {
   render() {
     return (
       <section className="list">
-        <ul>{[<li>Place # 1</li>, <li>Place # 2</li>]}</ul>
+        <ul>
+          {this.props.locations.map((place, index) => {
+            return <li key={index}>{place.name}</li>;
+          })}
+        </ul>
       </section>
     );
   }
